90a63fd83cbf371d611ac76afcc6f49af2be88a3
Thbop <75861321+thbop@users.noreply.github.com>
2024-05-15 20:01:53 -0400

    More mov instructions

--- main.c
+++ main.c
@@ -11,17 +11,17 @@ enum instructions {
     INS_MOV_IM_X  = 0x02,
     INS_MOV_IM_Y  = 0x03,
     INS_MOV_M_A   = 0x04,
-    INS_MOV_MX_A  = 0x05, // LD Addr+X           -> A
-    INS_MOV_MY_A  = 0x06, // LD Ptr+Y  -> Addr   -> A
-    INS_MOV_MXY_A = 0x07, // LD Ptr+Y  -> Addr+x -> A
-    INS_MOV_M_X   = 0x08,
-    INS_MOV_M_Y   = 0x09,
+    INS_MOV_M_X   = 0x05,
+    INS_MOV_M_Y   = 0x06,
+    INS_MOV_MX_A  = 0x07, // LD Addr+X           -> A
+    INS_MOV_MY_A  = 0x08, // LD Ptr+Y  -> Addr   -> A
+    INS_MOV_MXY_A = 0x09, // LD Ptr+Y  -> Addr+x -> A
     INS_MOV_A_M   = 0x0A,
-    INS_MOV_A_MX  = 0x0B, // ST A           -> Addr+X          
-    INS_MOV_A_MY  = 0x0C, // ST A -> Ptr+Y  -> Addr  
-    INS_MOV_A_MXY = 0x0D, // ST A -> Ptr+Y  -> Addr+x
-    INS_MOV_X_M   = 0x0E,
-    INS_MOV_Y_M   = 0x0F
+    INS_MOV_X_M   = 0x0B,
+    INS_MOV_Y_M   = 0x0C,
+    INS_MOV_A_MX  = 0x0D, // ST A           -> Addr+X          
+    INS_MOV_A_MY  = 0x0E, // ST A -> Ptr+Y  -> Addr  
+    INS_MOV_A_MXY = 0x0F, // ST A -> Ptr+Y  -> Addr+x
                           // Stack operations
 };
 
@@ -63,6 +63,12 @@ void execute(u8 ins) {
         case INS_MOV_M_A: {
             reg.A = memory[*fetch_bytes(1)];
         } break;
+        case INS_MOV_M_X: {
+            reg.X = memory[*fetch_bytes(1)];
+        } break;
+        case INS_MOV_M_Y: {
+            reg.Y = memory[*fetch_bytes(1)];
+        } break;
         case INS_MOV_MX_A: {
             reg.A = memory[*fetch_bytes(1)+reg.X];
         } break;
@@ -72,14 +78,26 @@ void execute(u8 ins) {
         case INS_MOV_MXY_A: {
             reg.A = memory[ memory[*fetch_bytes(1)+reg.X]+reg.Y ];
         } break;
+        case INS_MOV_A_M: {
+            memory[*fetch_bytes(1)] = reg.A;
+        } break;
+        case INS_MOV_X_M: {
+            memory[*fetch_bytes(1)] = reg.X;
+        } break;
+        case INS_MOV_Y_M: {
+            memory[*fetch_bytes(1)] = reg.Y;
+        } break;
             
     }
-    printf("$%x\n", reg.A);
 }
 
 int main() {
     initialize();
     memory[0x22] = 0x45;
-    execute( INS_MOV_IM_A );
+    execute( INS_MOV_IM_X );
+    memory[0x24] = 0x70;
+    execute( INS_MOV_X_M );
+
+    printf("$%x\n", memory[0x70]);
     return 0;
 }
